351ebb1142f5270dbf8c1ae5ac07d95e29d6a6cb
Phuc Hoang <phuc.hoangkien@gmail.com>
2023-11-24 10:50:23 +0700

    Add pause and play button

--- Assets/HeroEditor/Common/Scripts/SceneManager/ButtonManager.cs
+++ Assets/HeroEditor/Common/Scripts/SceneManager/ButtonManager.cs
@@ -2,9 +2,12 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 public class ButtonManager : MonoBehaviour
 {
+    [SerializeField] Sprite playImage;
+    [SerializeField] Sprite pauseImage;
     public void LoadLevelSelectScene()
     {
         SceneManager.LoadScene("LevelSelect");
@@ -13,5 +16,20 @@ public class ButtonManager : MonoBehaviour
     public void LoadLevelScene(string levelName)
     {
         SceneManager.LoadScene(levelName);
+    }
+
+    public void PauseButton(Button button)
+    {
+        if (Time.timeScale != 0)
+        {
+            Time.timeScale = 0;
+            button.GetComponent<Image>().sprite = playImage;
+        }
+        else
+        {
+            Time.timeScale = 1;
+            button.GetComponent<Image>().sprite = pauseImage;
+        }    
+                  
     }
 }
